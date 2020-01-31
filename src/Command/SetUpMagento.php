5071a7673a0f4a4d43fad5a55011aa0c712d1186
Maksym Zaporozhets <maksimz@default-value.com>
2020-01-31 17:26:36 +0200

    Fixed `setup:magento` for compatibility with new docker-compose files structure

--- src/Command/SetUpMagento.php
+++ src/Command/SetUpMagento.php
@@ -189,7 +189,7 @@ BASH
             } else {
                 $this->passthru(<<<BASH
                     cd $projectRoot
-                    docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d --build --force-recreate
+                    docker-compose -f docker-compose.yml -f docker-compose-prod.yml up -d --build --force-recreate
 BASH
                 );
             }
