bcaa13812678a60e86288dda5944d85a9b99854b
Serhii Raichuk <serhiir@default-value.com>
2020-04-30 17:48:16 +0300

    Added 'module:deploy-after-magento' command

--- src/Command/ModuleDeployAfterMagentoCommand.php
+++ src/Command/ModuleDeployAfterMagentoCommand.php
@@ -3,35 +3,99 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\CommandQuestion\Question\MysqlContainer;
+use App\Service\Filesystem;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ModuleDeployAfterMagentoCommand extends AbstractCommand
 {
-    private static $magentoDirectoriesAndFilesToClean = [
-        './app/etc/config.php',
-        './app/etc/config.php',
-        './generated/code/*',
-        './generated/metadata/*',
-        './var/di/*',
-        './var/generation/*',
-        './var/cache/*',
-        './var/page_cache/*',
-        './var/view_preprocessed/*',
-        './pub/static/*',
+    private const OPTION_FOLDER= 'folder';
 
-
+    /**
+     * Cleanup next directories
+     * @var array
+     */
+    private $magentoDirectoriesAndFilesToClean = [
+        'app/etc/config.php',
+        'app/etc/env.php',
+        'app/code/*',
+        'generated/code/*',
+        'generated/metadata',
+        'var/di/*',
+        'var/generation/*',
+        'var/cache/*',
+        'var/page_cache/*',
+        'var/view_preprocessed/*',
+        'pub/static/frontend/*',
     ];
 
     /**
+     * @var \App\Service\MagentoInstaller $magentoInstaller
+     */
+    private $magentoInstaller;
+    /**
+     * @var \App\Service\Filesystem $filesystem
+     */
+    private $filesystem;
+
+    /**
+     * ModuleDeployAfterMagentoCommand constructor.
+     * @param \App\Service\Filesystem $filesystem
+     * @param \App\Service\MagentoInstaller $magentoInstaller
+     * @param \App\Config\Env $env
+     * @param \App\Service\Shell $shell
+     * @param \App\CommandQuestion\QuestionPool $questionPool
+     * @param null $name
+     */
+    public function __construct(
+        \App\Service\Filesystem $filesystem,
+        \App\Service\MagentoInstaller $magentoInstaller,
+        \App\Config\Env $env,
+        \App\Service\Shell $shell,
+        \App\CommandQuestion\QuestionPool $questionPool,
+        $name = null)
+    {
+        parent::__construct($env, $shell, $questionPool, $name);
+        $this->magentoInstaller = $magentoInstaller;
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * @return void
      * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
      */
-    protected function configure()
+    protected function configure(): void
     {
         $this->setName('module:deploy-after-magento')
-            ->setDescription('<info>Re-install Magento 2 application, copy module files upgrade. Run \'setup:magento\' first</info>');
-            // @TODO: add help
-            // ->setHelp('');
+            ->setDescription(
+                '<info>Re-install Magento 2 application, copy modules from directory, run Magento deploy</info>'
+            )->addArgument(
+                self::OPTION_FOLDER,
+                InputArgument::REQUIRED,
+                'Modules folder directory'
+            )->setHelp(<<<'EOF'
+The <info>%command.name%</info> command clean up existing Magento, copied modules from the required folder, and run full Magento setup, including production mode, reindex and sample data.</info>
+You will be asked to select a DB container if it has not been provided.
+
+Usages:
+
+    <info>php bin/console module:deploy-after-magento "/misc/apps/modules" --mysql-container=mysql56</info>
+
+EOF);
+
+        parent::configure();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getQuestions(): array
+    {
+        return [
+            MysqlContainer::QUESTION
+        ];
     }
 
     /**
@@ -44,82 +108,125 @@ class ModuleDeployAfterMagentoCommand extends AbstractCommand
         $startTime = microtime(true);
 
         $projectRoot = getcwd();
-        $directoryName = basename($projectRoot);
+        $mainDomain = basename($projectRoot);
+        $modulesFolder = $input->getArgument(self::OPTION_FOLDER);
 
-        # Stage 1: copy files, deploy Sample Data, create database
         if (!file_exists('./app') || !is_dir('./app')) {
             $output->writeln('<error>Seems we\'re not inside the Magento project</error>');
-            exit;
+            exit(1);
         }
 
-        // @TODO: use composer require to install from the repository
-        // @TODO: add directory parameter with all modules
-        // @TODO: find all etc/module.xml, get vendor-module, create folders, copy
+        if (!is_dir($modulesFolder)) {
+            $output->writeln('<error>Modules directory is not exist.</error>');
+            exit(1);
+        }
+
+        $modules = $this->detectModules($modulesFolder);
+
+        $mysqlContainer = $this->ask(MysqlContainer::QUESTION, $input, $output);
+
+        # Stage 0: clean Magento 2, install Magento application
 
         $output->writeln('<info>Cleanup Magento 2 application...</info>');
 
-        $this->dockerExec("sh -c \"rm -rf dirlisthere\"");
-        $directoriesToClean = [
+        $filesAndFolderToRemove = implode($this->magentoDirectoriesAndFilesToClean, ' ');
+        $this->shell->dockerExec("sh -c \"rm -rf {$filesAndFolderToRemove}\"", $mainDomain);
 
-        ];
+        $currentUser = get_current_user();
+        $userGroup = filegroup($this->filesystem->getDirPath(Filesystem::DIR_PROJECT_TEMPLATE));
+        $this->shell->sudoPassthru("chown -R $currentUser:$userGroup ./");
 
-        // @TODO: additional directories to remove
-        // @TODO: do we need additional external packages to require? Or probably must get them from the composer.json of the installed modules???????????
+        $this->magentoInstaller->refreshDbAndInstall($mainDomain);
+        $this->magentoInstaller->updateMagentoConfig($mainDomain);
 
-        passthru(<<<BASH
-            rm -rf ./app/code/Vendor
-            rm -rf ./pub/media/vendor/
-            mkdir -p ./app/code/Vendor
-BASH
-        );
+        # Stage 1: deploy Sample Data, run setup upgrade
 
         $output->writeln('<info>Deploy Sample Data...</info>');
 
         if (!file_exists('./var/.sample-data-state.flag')) {
-            $this->dockerExec('php bin/magento sampledata:deploy');
+            $this->shell->dockerExec('php bin/magento sampledata:deploy', $mainDomain);
         }
 
-        passthru('chmod +x ./bin/magento');
+        $this->shell->dockerExec('php bin/magento setup:upgrade', $mainDomain);
 
-        # Stage 2: install application, switch to the production mode
-        $output->writeln('<info>Refresh DB and install...</info>');
-        $this->refreshDbAndInstall();
+        # Stage 2: run reindex, switch to the production mode
 
-        $this->updateMagentoConfig();
+        $output->writeln('<info>Running reindex, switching to the production mode...</info>');
 
-        $this->dockerExec('php bin/magento cache:disable full_page block_html')
-            ->dockerExec('php bin/magento deploy:mode:set production')
-            ->dockerExec('php bin/magento indexer:reindex');
+        $this->shell->dockerExec('php bin/magento indexer:reindex', $mainDomain);
+        $this->shell->dockerExec('php bin/magento deploy:mode:set production', $mainDomain);
 
-        $output->writeln('<info>Copy and commit Detailed Review...</info>');
+        # Stage 3: copy modules, commit changes, run setup:upgrade with modules
 
-        passthru(<<<BASH
-            cp -r /misc/apps/drm2/Vendor/* ./app/code/Vendor/
-            # rm -rf ./app/code/Vendor/Module
+        $output->writeln('<info>Copy modules, run setup:upgrade...</info>');
+
+        foreach ($modules as $vendorName => $vendorModules) {
+            $vendorDirectory = 'app' .
+                DIRECTORY_SEPARATOR .
+                'code' .
+                DIRECTORY_SEPARATOR .
+                $vendorName;
+            $this->shell->dockerExec("mkdir -p {$vendorDirectory} ", $mainDomain);
+
+            foreach ($vendorModules as $moduleName => $moduleData) {
+                $this->shell->passthru("cp -R {$moduleData['path']} {$vendorDirectory}");
+            }
+        }
+
+        $output->writeln('<info>Commit changes...</info>');
+
+        $this->shell->passthru(<<<BASH
             git config core.fileMode false
-            git add ./app/code/Vendor
-            git add -u ./app/code/Vendor
+            git add ./app/code/*
+            git add -u ./app/code/*
             git commit -m "New build"
 BASH
         );
 
-        $output->writeln('<info>Run setup:upgrade with DRM2...</info>');
-        // passthru('rm -rf ./var/di/'); // bug with compilation in Magento 2.1 only ?
-        $this->dockerExec('php bin/magento setup:upgrade');
+        $this->shell->dockerExec('php bin/magento setup:upgrade', $mainDomain);
 
-        $output->writeln('<info>Switching to the production mode...</info>');
+        # Stage 4: switch magento 2 to the production mode, run final reindex
 
-        $this->dockerExec('php bin/magento deploy:mode:set production');
+        $output->writeln('<info>Final reindex...</info>');
 
-        $output->writeln('<info>Running final reindex...</info>');
-        $this->dockerExec('php bin/magento indexer:reindex');
+        $this->shell->dockerExec('php bin/magento deploy:mode:set production', $mainDomain);
+        $this->shell->dockerExec('php bin/magento indexer:reindex', $mainDomain);
 
         $endTime = microtime(true);
         $minutes = floor(($endTime - $startTime) / 60);
         $seconds = round($endTime - $startTime - $minutes * 60, 2);
-        $output->writeln("<info>Completed in $minutes minutes $seconds seconds!</info>");
-        $baseUrl = 'https://' . $directoryName;
-        $output->writeln("<info>Frontend: $baseUrl</info>");
-        $output->writeln("<info>Admin Panel: $baseUrl</info>");
+        $output->writeln("<info>Completed in {$minutes} minutes {$seconds} seconds!</info>");
+    }
+
+    /**
+     * @param $baseDirectory
+     * @return array
+     */
+    private function detectModules($baseDirectory): array
+    {
+        $modules = [];
+
+        foreach (array_diff(scandir($baseDirectory), array('..', '.', '.git')) as $directoryChild) {
+            $detectedDirectory = $baseDirectory . DIRECTORY_SEPARATOR . $directoryChild;
+            if (file_exists($detectedDirectory. DIRECTORY_SEPARATOR . 'registration.php') &&
+                file_exists($detectedDirectory. DIRECTORY_SEPARATOR . 'etc/module.xml')) {
+                $moduleName = simplexml_load_file("{$detectedDirectory}/etc/module.xml")->{module}->attributes()->name;
+
+                if ($moduleName) {
+                    $explodedModuleName = explode('_', (string)$moduleName);
+                    $modules[$explodedModuleName[0]][$explodedModuleName[1]] = [
+                        'path' => $detectedDirectory
+                    ];
+                }
+
+                continue;
+            }
+
+            if (is_dir($detectedDirectory)) {
+                $modules = $this->detectModules($detectedDirectory);
+            }
+        }
+
+        return $modules;
     }
 }
