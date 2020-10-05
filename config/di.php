c7803bb67d0f8472f2a4c35366ad2d9c0c76b548
Maksym Zaporozhets <maksimz@default-value.com>
2020-10-05 10:24:45 +0300

    Updated dependencies and code for compatibility with Symfony Dotenv 5.1.*

--- config/di.php
+++ config/di.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 return [
-    \Symfony\Component\Dotenv\Dotenv::class => DI\autowire()->constructorParameter('usePutenv', true)
+    // \Symfony\Component\Dotenv\Dotenv::class => DI\autowire()->constructorParameter('usePutenv', true)
 ];
