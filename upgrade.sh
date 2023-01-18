ac2b5d22d706088ae8fab5a8a7daf6bb0d790edb
Maksym Zaporozhets <maksimz@default-value.com>
2023-01-18 18:53:56 +0200

    Added copyright

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#
+# Copyright (c) Default Value LLC.
+# This source file is subject to the License https://github.com/DefaultValue/dockerizer_for_php/LICENSE.txt
+# Do not change this file if you want to upgrade the tool to the newer versions in the future
+# Please, contact us at https://default-value.com/#contact if you wish to customize this tool
+# according to you business needs
+#
+
 set -e
 
 sudo apt purge php* -y
